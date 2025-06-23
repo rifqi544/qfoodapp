acc5f5568f43ac99781182397a7633a379273622
rifqi544 <rifqibusiness56@gmail.com>
2025-06-23 14:15:28 +0700

    db.php

--- db.php
+++ db.php
@@ -1,6 +1,11 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "qfood");
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$name = getenv("DB_NAME");
+$port = getenv("DB_PORT");
+
+$conn = new mysqli($host, $user, $pass, $name, $port);
+
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-?>
