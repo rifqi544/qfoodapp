86a5bb5c3a39e7682e05dd1714a89b5a2dc8a972
rifqi544 <rifqibusiness56@gmail.com>
2025-06-23 14:30:00 +0700

    db.php

--- db.php
+++ db.php
@@ -1,11 +1,13 @@
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
-$name = getenv("DB_NAME");
-$port = getenv("DB_PORT");
+<?php
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$name = getenv('DB_NAME');
 
 $conn = new mysqli($host, $user, $pass, $name, $port);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
+?>
