483a5ebef1d81771e6e0d4e084afb822a2d02529
rifqi544 <rifqibusiness56@gmail.com>
2025-06-23 14:34:35 +0700

    db.php

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$name = getenv('DB_NAME');
+$host = 'centerbeam.proxy.rlwy.net'; // dari "host" di URL MySQL Railway
+$port = 58148;                        // dari "port" di URL
+$user = 'root';                      // dari "user" di URL
+$pass = 'slEkPzynjxRTjrHNOiFYPvuZfgdPPSae';                // dari "password" di URL, bisa klik "show" untuk lihat
+$db   = 'railway';                   // biasanya nama DB-nya memang 'railway'
 
-$conn = new mysqli($host, $user, $pass, $name, $port);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
